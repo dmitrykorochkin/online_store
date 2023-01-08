5c839f5c9d28c5064ff7a2ca8db385a336105d5b
Dmitry Korochkin <abramslost@gmail.com>
2023-01-08 19:15:28 +0300

    add input chackra

--- app/components/layout/header/search/Search.tsx
+++ app/components/layout/header/search/Search.tsx
@@ -1,4 +1,5 @@
-import { Input } from 'antd'
+import { SearchIcon } from '@chakra-ui/icons'
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { FC, useState } from 'react'
 
 import styles from './Search.module.scss'
@@ -7,10 +8,18 @@ const Search: FC = () => {
 	const [searchTerm, setSearchTerm] = useState('')
 	return (
 		<div className={styles.search}>
-			<Input.Search
-				onChange={e => setSearchTerm(e.target.value)}
-				value={searchTerm}
-			/>
+			<InputGroup>
+				<InputLeftElement
+					pointerEvents='none'
+					children={<SearchIcon color='gray.300' />}
+				/>
+				<Input
+					variant='flushed'
+					placeholder='Search'
+					onChange={e => setSearchTerm(e.target.value)}
+					value={searchTerm}
+				/>
+			</InputGroup>
 		</div>
 	)
 }
