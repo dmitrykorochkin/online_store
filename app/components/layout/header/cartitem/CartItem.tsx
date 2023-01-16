9e8dfaddbf77bb30da6999d8b097c24b77ae2b12
Dmitry Korochkin <abramslost@gmail.com>
2023-01-16 22:17:11 +0300

    add CartItem

--- app/components/layout/header/cartitem/CartItem.tsx
+++ app/components/layout/header/cartitem/CartItem.tsx
@@ -1,14 +1,29 @@
-import { ICartItem } from '@/types/cart.interface'
+import Image from 'next/image'
 import { FC } from 'react'
 
-const CartItem: FC<{item: ICartItem}> = ({item}) => {
-  return (
-    <div>
-      <Image>
-        src={}
-      </Image>
-    </div>
-  )
+import styles from './Cart.module.scss'
+import { ICartItem } from '@/types/cart.interface'
+
+const CartItem: FC<{ item: ICartItem }> = ({ item }) => {
+	return (
+		<div className={styles.item}>
+			<Image>
+				src={item.product.images[0]}
+				width={100}
+				height={100}
+				alt={item.product}
+			</Image>
+			<div>
+				<div className={styles.name}>{item.product.name}</div>
+				<div className={styles.price}>
+					{new Intl.NumberFormat(undefined, {
+						style: 'currency',
+						currency: 'USD'
+					}).format(item.product.price)}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default CartItem
