245f38084a2637f81c84b2bcd75fd1f78a3ee000
Dmitry Korochkin <abramslost@gmail.com>
2023-01-22 13:12:42 +0300

    add product

---
+++ app/data/cart.data.ts
@@ -0,0 +1,5 @@
+import { ICartItem } from '@/types/cart.interface';
+export const cart : ICartItem[] = [{
+  id:1
+  quantity:1
+}]
---
+++ app/data/product.data.ts
@@ -0,0 +1,13 @@
+import { IProduct } from "@/types/product.inteface"
+
+export const products: IProduct[] = [{
+  id:1,
+  descriptions: 'fdfsddsfsdfdsffsdffd sfsd fsdf sd',
+  name: 'Capuchino',
+  images: [
+    
+  ],
+  price: 9,
+  reviews: []
+}
+]
