7f86b5e5c57a897ae7d2d13295d726bcbb250b47
Dmitry Korochkin <abramslost@gmail.com>
2023-01-23 13:46:37 +0300

    add open and closed cart

--- app/components/layout/header/cart/Cart.tsx
+++ app/components/layout/header/cart/Cart.tsx
@@ -1,19 +1,25 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import styles from './Cart.module.scss'
 import { cart } from '@/data/cart.data'
 import CartItem from './cartitem/CartItem'
 
+
 const Cart: FC = () => {
+
+	const [isOpen, setIsOpen] = useState(false)
+
 	return <div className={styles['wrapper-cart']}>
-		<button className={styles.heading}>
+		<button className={styles.heading} onClick = {() => setIsOpen(!isOpen)}>
 			<span className={styles.badge}>1</span>
 			<span className={styles.text}>MY BASKET</span>
 		</button>
-		<div className={styles.cart}>
-			{cart.map(item => (
-				<CartItem item={item} key={item.id}/>
-			))}
-		</div>
+	{isOpen && (
+				<div className={styles.cart}>
+				{cart.map(item => (
+					<CartItem item={item} key={item.id}/>
+				))}
+			</div>
+	)}
 	</div>
 }
 
