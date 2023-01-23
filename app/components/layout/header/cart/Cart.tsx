80ca6aa4228eab39a9ed1c14c39ae3c7584ca8e0
Dmitry Korochkin <abramslost@gmail.com>
2023-01-23 15:43:13 +0300

    add cart to chakra ui

--- app/components/layout/header/cart/Cart.tsx
+++ app/components/layout/header/cart/Cart.tsx
@@ -1,25 +1,46 @@
-import { FC, useState } from 'react'
+import { FC, useState, useRef } from 'react'
 import styles from './Cart.module.scss'
 import { cart } from '@/data/cart.data'
 import CartItem from './cartitem/CartItem'
+import { Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody,Button, DrawerFooter } from '@chakra-ui/react'
 
 
 const Cart: FC = () => {
 
 	const [isOpen, setIsOpen] = useState(false)
+	const btnRef = useRef<HTMLButtonElement>(null)
 
 	return <div className={styles['wrapper-cart']}>
-		<button className={styles.heading} onClick = {() => setIsOpen(!isOpen)}>
+		<button className={styles.heading} onClick = {() => setIsOpen(!isOpen)} ref={btnRef}>
 			<span className={styles.badge}>1</span>
 			<span className={styles.text}>MY BASKET</span>
 		</button>
-	{isOpen && (
+
+		<Drawer
+		isOpen={isOpen}
+		placement='right'
+		onClose={() => setIsOpen(false)}
+		finalFocusRef={btnRef}
+	>
+		<DrawerOverlay />
+		<DrawerContent>
+			<DrawerCloseButton />
+			<DrawerHeader>My basket</DrawerHeader>
+
+			<DrawerBody>
 				<div className={styles.cart}>
-				{cart.map(item => (
-					<CartItem item={item} key={item.id}/>
-				))}
-			</div>
-	)}
+					{cart.map(item => (
+						<CartItem item={item} key={item.id}/>
+					))}
+				</div>
+			</DrawerBody>
+
+			<DrawerFooter>
+				<Button colorScheme='green'>Checkout</Button>
+			</DrawerFooter>
+		</DrawerContent>
+	</Drawer>
+
 	</div>
 }
 
