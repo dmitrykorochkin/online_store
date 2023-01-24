8a55adbf4a3d84800af4861c1024fcef0573515b
Dmitry Korochkin <abramslost@gmail.com>
2023-01-24 16:07:34 +0300

    add inputNumber

--- app/components/layout/header/cart/cartitem/cart-actions/CartActions.tsx
+++ app/components/layout/header/cart/cartitem/cart-actions/CartActions.tsx
@@ -1,34 +1,36 @@
-
 import { AddIcon, MinusIcon } from '@chakra-ui/icons'
 import { Button, HStack, Input, useNumberInput } from '@chakra-ui/react'
 import { FC } from 'react'
 
-const CartActions:FC = () => {
-  const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
-  useNumberInput({
-    step: 1,
-    defaultValue: 1,
-    min: 0,
-    max: 1000,
-  })
+const CartActions: FC = () => {
+	const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+		useNumberInput({
+			step: 1,
+			defaultValue: 1,
+			min: 0,
+			max: 1000
+		})
 
-const inc = getIncrementButtonProps()
-const dec = getDecrementButtonProps()
-const input = getInputProps()
+	const inc = getIncrementButtonProps()
+	const dec = getDecrementButtonProps()
+	const input = getInputProps()
 
-return (
-  <div>
-    <HStack maxW='320px'>
-      <Button {...inc}>
-        <AddIcon/>
-      </Button>
-        <Input {...input} focusBorderColor='green.400'/>
-      <Button {...dec}>
-        <MinusIcon/>
-      </Button>
-    </HStack>
-  </div>
-)
+	return (
+		<div>
+			<HStack maxW='320px'>
+				<Button {...inc}>
+					<AddIcon />
+				</Button>
+				<Input {...input} focusBorderColor='green.400' />
+				<Button {...dec}>
+					<MinusIcon />
+				</Button>
+			</HStack>
+			<Button variant={'link'} color='red.400' marginTop={2} size='sm'>
+				Remove
+			</Button>
+		</div>
+	)
 }
 
 export default CartActions
